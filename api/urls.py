93cf39be1a596220ea0a6ee35552a3e8f91919b4
Takuho3650 <85665691+Takuho3650@users.noreply.github.com>
2024-09-03 14:00:52 +0900

    stuffのままになっていたurlを修正

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from . import apis
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('stuff-category', apis.StaffCategoryViewSet)
-router.register('stuff-plan', apis.StaffPlanViewSet)
-router.register('stuff-voter', apis.StaffVoterViewSet)
-router.register('stuff-votelog', apis.StaffVoteLogViewSet)
-router.register('stuff-latestvote', apis.StaffLatestVoteViewSet)
+router.register('staff-category', apis.StaffCategoryViewSet)
+router.register('staff-plan', apis.StaffPlanViewSet)
+router.register('staff-voter', apis.StaffVoterViewSet)
+router.register('staff-votelog', apis.StaffVoteLogViewSet)
+router.register('staff-latestvote', apis.StaffLatestVoteViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
